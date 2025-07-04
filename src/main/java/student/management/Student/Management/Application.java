aec92b045158b9a0a6b98e1252eb9598a7e09016
Msk-Lag <zapper2234@gmail.com>
2025-07-04 14:27:46 +0900

    23_MyBatisを使ってCRUDのRead処理を実装

--- src/main/java/student/management/Student/Management/Application.java
+++ src/main/java/student/management/Student/Management/Application.java
@@ -5,43 +5,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 
 public class Application {
 
-	@Autowired
-	private StudentRepository repository;
+    @Autowired
+    private StudentRepository repository;
+    @Autowired
+    private StudentCourseRepository courseRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 
-	
+    @GetMapping("/studentList")
+    public List<Student> getStudentList() {
+        return repository.search();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-
-	@GetMapping("/student")
-	public String getStudent(@RequestParam String name){
-		Student student = repository.searchByName(name);
-		return student.getName() + " " + student.getAge() + "歳";
-	}
-
-	@PostMapping("/student")
-	public  void registerStudent(String name,int age){
-		repository.registerStudent(name,age);
-	}
-
-	@PatchMapping ("/student")
-	public void updateStudentName(String name, int age){
-		repository.updateStudent(name, age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
-
-	}
+    @GetMapping("/studentCourseList")
+    public List<StudentCourse> getStudentCourseList() {
+        return courseRepository.courseSearch();
+    }
 
 
 }
