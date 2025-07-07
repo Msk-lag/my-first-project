04b603b73cea843dba376fe5c4db13545425b9fe
Msk-Lag <zapper2234@gmail.com>
2025-07-07 14:50:25 +0900

    25_Read処理のConverter部分実装.修正

--- src/main/java/student/management/Student/Management/Repository/StudentRepository.java
+++ src/main/java/student/management/Student/Management/Repository/StudentRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Mapper
 public interface StudentRepository {
-    @Select("SELECT*FROM students")
+    @Select("SELECT * FROM students")
     List<Student> search();
 
 }
