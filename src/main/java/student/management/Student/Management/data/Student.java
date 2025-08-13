e50e13ebd7d45b19ce249f49b78d172ef7828971
Msk-Lag <zapper2234@gmail.com>
2025-08-13 11:59:35 +0900

    36_SpringBootでの例外処理

--- src/main/java/student/management/Student/Management/data/Student.java
+++ src/main/java/student/management/Student/Management/data/Student.java
@@ -1,6 +1,7 @@
 package student.management.Student.Management.data;
 
 
+import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,14 +11,32 @@ import lombok.Setter;
 public class Student {
 
     private String id;
+
+
     private String fullName;
+
+
     private String furigana;
+
+
     private String nickName;
+
+    @Email
     private String email;
+
+
     private String address;
+
+
     private int age;
+
+
     private String gender;
+
+
     private String remark;
+
+
     private boolean isDeleted;
 
 }
