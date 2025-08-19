6c67e1faff1c3694ae4af3832da527b0e20c0a9e
Msk-Lag <zapper2234@gmail.com>
2025-08-19 16:31:44 +0900

    41_Controllerのテストと入力チェックのテスト

--- src/main/java/student/management/Student/Management/data/Student.java
+++ src/main/java/student/management/Student/Management/data/Student.java
@@ -3,6 +3,8 @@ package student.management.Student.Management.data;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,27 +13,32 @@ import lombok.Setter;
 @Setter
 public class Student {
 
+    @NotBlank
+    @Pattern(
+            regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$",
+            message = "UUIDを使用してください")
     private String id;
 
-
+    @NotBlank
     private String fullName;
 
-
+    @NotBlank
     private String furigana;
 
-
+    @NotBlank
     private String nickName;
 
+    @NotBlank
     @Email
     private String email;
 
-
+    @NotBlank
     private String address;
 
 
     private int age;
 
-
+    @NotBlank
     private String gender;
 
 
